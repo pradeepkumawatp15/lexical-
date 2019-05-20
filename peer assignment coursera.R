114fd99e84b0344dad86ab5a6b638e742ade29da
pradeepkumawatp15 <40435750+pradeepkumawatp15@users.noreply.github.com>
2019-05-20 16:28:14 +0530

    Add files via upload

---
+++ peer assignment coursera.R
@@ -0,0 +1,33 @@
+makeCacheMatrix <- function(x = matrix())
+  inv <- NULL                         
+  set <- function(y) {                  
+    x <<- y                           
+    inv <<- NULL                      
+  }
+  get <- function() x                     
+  
+  ## assigns value of inv in parent environment
+  setinverse <- function(inverse) inv <<- inverse 
+  
+  ## gets the value of inv where called
+  getinverse <- function() inv                     
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer 
+  ## to the functions with the $ operator
+
+
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then cacheSolve will retrieve the inverse from the cache
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
+}
